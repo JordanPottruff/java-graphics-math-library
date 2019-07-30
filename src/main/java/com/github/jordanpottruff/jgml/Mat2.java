60b362f724cd0cf156e61f8d4f54456d3a6178af
jordanpottruff <jordanpottruff@gmail.com>
2019-07-30 00:42:16 -0700

    Add Mat2 class, including a transformation builder.

---
+++ src/main/java/com/github/jordanpottruff/jgml/Mat2.java
@@ -0,0 +1,184 @@
+package com.github.jordanpottruff.jgml;
+
+/**
+ * A matrix of dimensions 2x2.
+ */
+public class Mat2 extends MatN {
+
+    /**
+     * Constructs a Mat2 from a two-dimensional array of elements. The outer-array must contain two
+     * inner-arrays, with each being of length two. The order of the 2D array is preserved.
+     *
+     * @param array the 2D array of elements.
+     * @throws IllegalArgumentException if the outer-array does not contain two inner-arrays.
+     * @throws IllegalArgumentException if the inner-arrays are not all of length two.
+     */
+    public Mat2(double[][] array) {
+        super(array);
+        Util.verifyExactDimension(array, 2, 2);
+    }
+
+    /**
+     * Constructs a Mat2 from a matrix object. The matrix must contain two rows and two columns. The
+     * order of the elements will be preserved.
+     *
+     * @param mat the matrix object.
+     * @throws IllegalArgumentException if the matrix object is not of dimension 2x2.
+     */
+    public Mat2(Mat mat) {
+        super(mat);
+        Util.verifyExactDimension(mat.toArray(), 2, 2);
+    }
+
+    /**
+     * Constructs a Mat2 from three Vec2 objects. The vectors will create the column vectors of the
+     * new matrix.
+     *
+     * @param vec1 the first column vector.
+     * @param vec2 the second column vector.
+     */
+    public Mat2(Vec2 vec1, Vec2 vec2) {
+        super(new double[][]{vec1.toArray(), vec2.toArray()});
+    }
+
+    /**
+     * Creates a 3x3 identity matrix.
+     *
+     * @return the identity matrix.
+     */
+    public static Mat2 createIdentityMatrix() {
+        return new Mat2(Util.identity(2));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Mat2 invert() {
+        return new Mat2(super.invert());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Mat2 add(Mat mat) {
+        return new Mat2(super.add(mat));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Mat2 subtract(Mat mat) {
+        return new Mat2(super.subtract(mat));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Mat2 scale(double scalar) {
+        return new Mat2(super.scale(scalar));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Mat2 multiply(MatN mat) {
+        return new Mat2(super.multiply(mat));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Mat2 inverse() {
+        return new Mat2(super.inverse());
+    }
+
+    /**
+     * A Mat2 builder that provides methods to construct a transformation matrix. The order of
+     * execution of operations follows the order that the methods were called on the builder object.
+     */
+    public static class TransformBuilder {
+
+        private Mat2 matrix;
+
+        /**
+         * Creates a new transformation.
+         */
+        public TransformBuilder() {
+            matrix = createIdentityMatrix();
+        }
+
+        /**
+         * Scales in both axes.
+         *
+         * @param x how much to scale in the x-axis.
+         * @param y how much to scale in the y-axis.
+         * @return the transformed builder.
+         */
+        public TransformBuilder scale(double x, double y) {
+            double[][] operation = createIdentityMatrix().toArray();
+            operation[0][0] = x;
+            operation[1][1] = y;
+            applyOperation(operation);
+            return this;
+        }
+
+        /**
+         * Rotates (along the invisible third axis perpendicular to the view plane).
+         *
+         * @param radians the amount to rotate by, in radians.
+         * @return the transformed builder.
+         */
+        public TransformBuilder rotate(double radians) {
+            double cos = Math.cos(radians);
+            double sin = Math.sin(radians);
+            double[][] operation = createIdentityMatrix().toArray();
+            operation[0][0] = cos;
+            operation[1][0] = -sin;
+            operation[0][1] = sin;
+            operation[1][1] = cos;
+            applyOperation(operation);
+            return this;
+        }
+
+        /**
+         * Shears the x-axis based on the y factor.
+         *
+         * @param y shear factor for y-axis.
+         * @return the transformed builder.
+         */
+        public TransformBuilder shearX(double y) {
+            double[][] operation = createIdentityMatrix().toArray();
+            operation[1][0] = y;
+            applyOperation(operation);
+            return this;
+        }
+
+        /**
+         * Shears the y-axis based on x factor.
+         *
+         * @param x shear factor for x-axis.
+         * @return the transformed builder.
+         */
+        public TransformBuilder shearY(double x) {
+            double[][] operation = createIdentityMatrix().toArray();
+            operation[0][1] = x;
+            applyOperation(operation);
+            return this;
+        }
+
+        /**
+         * Creates the transformation matrix.
+         *
+         * @return the transformation matrix.
+         */
+        public Mat2 build() {
+            return new Mat2(matrix);
+        }
+
+        private void applyOperation(double[][] operation) {
+            Mat2 operationMat = new Mat2(operation);
+            matrix = operationMat.multiply(matrix);
+        }
+
+    }
+}
---
+++ src/test/java/com/github/jordanpottruff/jgml/Mat2Test.java
@@ -0,0 +1,91 @@
+package com.github.jordanpottruff.jgml;
+
+import org.junit.Test;
+
+import static com.github.jordanpottruff.jgml.MatMNTest.assertMatricesEqual;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Mat2Test {
+    private static final double ERROR_MARGIN = .001;
+
+    private static final Mat2 MAT_A = new Mat2(new double[][]{{1.0, 2.0}, {5.0, 6.0}});
+    private static final Mat2 MAT_B = new Mat2(new double[][]{{5.0, 10.0}, {25.0, 30.0}});
+    private static final Mat2 MAT_C = new Mat2(new double[][]{{5.0, 1.0}, {1.0, 5.0}});
+
+    @Test
+    public void testInvert() {
+        Mat2 inverted = new Mat2(new double[][]{{-1.0, -2.0}, {-5.0, -6.0}});
+        assertMatricesEqual(inverted, MAT_A.invert(), ERROR_MARGIN);
+    }
+
+    @Test
+    public void testAdd() {
+        Mat2 sum = new Mat2(new double[][]{{6.0, 12.0}, {30.0, 36.0}});
+        assertMatricesEqual(sum, MAT_A.add(MAT_B), ERROR_MARGIN);
+    }
+
+    @Test
+    public void testSubtract() {
+        Mat2 diff = new Mat2(new double[][]{{4.0, 8.0}, {20.0, 24.0}});
+        assertMatricesEqual(diff, MAT_B.subtract(MAT_A), ERROR_MARGIN);
+    }
+
+    @Test
+    public void testScale() {
+        assertMatricesEqual(MAT_B, MAT_A.scale(5.0), ERROR_MARGIN);
+    }
+
+    @Test
+    public void testDeterminant() {
+        assertEquals(-4.0, MAT_A.determinant(), ERROR_MARGIN);
+    }
+
+    @Test
+    public void testInverse() {
+        Mat2 inverse = new Mat2(new double[][]{{0.2083, -0.0417}, {-0.0417, 0.2083}});
+        assertMatricesEqual(inverse, MAT_C.inverse(), ERROR_MARGIN);
+    }
+
+    @Test
+    public void testBuilder_scale() {
+        Mat2 scaleMat = new Mat2.TransformBuilder().scale(5.0, 3.0).build();
+        Mat2 expected = new Mat2(new double[][]{{5.0, 0.0}, {0.0, 3.0}});
+        assertMatricesEqual(expected, scaleMat, ERROR_MARGIN);
+    }
+
+    @Test
+    public void testBuilder_rotate() {
+        Mat2 rotateXMat = new Mat2.TransformBuilder().rotate(Math.PI / 2).build();
+        Mat2 expected = new Mat2(new double[][]{{0.0, 1.0}, {-1.0, 0.0}});
+        assertMatricesEqual(expected, rotateXMat, ERROR_MARGIN);
+    }
+
+    @Test
+    public void testBuilder_shearX() {
+        Mat2 shearXMat = new Mat2.TransformBuilder().shearX(1.0).build();
+        Mat2 expected = new Mat2(new double[][]{{1.0, 0.0}, {1.0, 1.0}});
+        assertMatricesEqual(expected, shearXMat, ERROR_MARGIN);
+    }
+
+    @Test
+    public void testBuilder_shearY() {
+        Mat2 shearYMat = new Mat2.TransformBuilder().shearY(1.0).build();
+        Mat2 expected = new Mat2(new double[][]{{1.0, 1.0}, {0.0, 1.0}});
+        assertMatricesEqual(expected, shearYMat, ERROR_MARGIN);
+    }
+
+    @Test
+    public void testBuilder_chainOperations() {
+        // Variety of operations
+        Mat2 transformation =
+                new Mat2.TransformBuilder().rotate(Math.PI / 4).scale(4.0, 2.0).build();
+        Mat2 expected = new Mat2(new double[][]{{2.8284, 1.4142}, {-2.8284, 1.4142}});
+        assertMatricesEqual(expected, transformation, ERROR_MARGIN);
+
+        // Reverse operations
+        Mat2 reverse =
+                new Mat2.TransformBuilder().rotate(Math.PI / 4).scale(5.0, 1.0).scale(1.0 / 5.0,
+                        1.0).rotate(-Math.PI / 4).build();
+        assertMatricesEqual(Mat2.createIdentityMatrix(), reverse, ERROR_MARGIN);
+    }
+}
