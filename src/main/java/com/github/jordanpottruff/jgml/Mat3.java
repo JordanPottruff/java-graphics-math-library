d2aaba73ee26d63341e22975a4cd7f014afd2029
jordanpottruff <jordanpottruff@gmail.com>
2019-07-30 00:27:40 -0700

    Add Mat3 class, including affine transformation builder.

---
+++ src/main/java/com/github/jordanpottruff/jgml/Mat3.java
@@ -0,0 +1,200 @@
+package com.github.jordanpottruff.jgml;
+
+/**
+ * A matrix of dimensions 3x3.
+ */
+public class Mat3 extends MatN {
+
+    /**
+     * Constructs a Mat3 from a two-dimensional array of elements. The outer-array must contain
+     * three inner-arrays, with each being of length three. The order of the 2D array is preserved.
+     *
+     * @param array the 2D array of elements.
+     * @throws IllegalArgumentException if the outer-array does not contain three inner-arrays.
+     * @throws IllegalArgumentException if the inner-arrays are not all of length three.
+     */
+    public Mat3(double[][] array) {
+        super(array);
+        Util.verifyExactDimension(array, 3, 3);
+    }
+
+    /**
+     * Constructs a Mat3 from a matrix object. The matrix must contain three rows and three columns.
+     * The order of the elements will be preserved.
+     *
+     * @param mat the matrix object.
+     * @throws IllegalArgumentException if the matrix object is not of dimension 3x3.
+     */
+    public Mat3(Mat mat) {
+        super(mat);
+        Util.verifyExactDimension(mat.toArray(), 3, 3);
+    }
+
+    /**
+     * Constructs a Mat3 from three Vec3 objects. The vectors will create the column vectors of the
+     * new matrix.
+     *
+     * @param vec1 the first column vector.
+     * @param vec2 the second column vector.
+     * @param vec3 the third column vector.
+     */
+    public Mat3(Vec3 vec1, Vec3 vec2, Vec3 vec3) {
+        super(new double[][]{vec1.toArray(), vec2.toArray(), vec3.toArray()});
+    }
+
+    /**
+     * Creates a 3x3 identity matrix.
+     *
+     * @return the identity matrix.
+     */
+    public static Mat3 createIdentityMatrix() {
+        return new Mat3(Util.identity(3));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Mat3 invert() {
+        return new Mat3(super.invert());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Mat3 add(Mat mat) {
+        return new Mat3(super.add(mat));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Mat3 subtract(Mat mat) {
+        return new Mat3(super.subtract(mat));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Mat3 scale(double scalar) {
+        return new Mat3(super.scale(scalar));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Mat3 multiply(MatN mat) {
+        return new Mat3(super.multiply(mat));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Mat3 inverse() {
+        return new Mat3(super.inverse());
+    }
+
+    /**
+     * A Mat3 builder that provides methods to construct an affine transformation. The order of
+     * execution of operations follows the order that the methods were called on the builder object.
+     */
+    public static class TransformBuilder {
+
+        private Mat3 matrix;
+
+        /**
+         * Creates a new transformation.
+         */
+        public TransformBuilder() {
+            matrix = createIdentityMatrix();
+        }
+
+        /**
+         * Scales in both axes.
+         *
+         * @param x how much to scale in the x-axis.
+         * @param y how much to scale in the y-axis.
+         * @return the transformed builder.
+         */
+        public TransformBuilder scale(double x, double y) {
+            double[][] operation = createIdentityMatrix().toArray();
+            operation[0][0] = x;
+            operation[1][1] = y;
+            applyOperation(operation);
+            return this;
+        }
+
+        /**
+         * Translates in both axes.
+         *
+         * @param x how much to translate along the x-axis.
+         * @param y how much to translate along the y-axis.
+         * @return the transformed builder.
+         */
+        public TransformBuilder translate(double x, double y) {
+            double[][] operation = createIdentityMatrix().toArray();
+            operation[2][0] = x;
+            operation[2][1] = y;
+            applyOperation(operation);
+            return this;
+        }
+
+        /**
+         * Rotates (along the invisible third axis perpendicular to the view plane).
+         *
+         * @param radians the amount to rotate by, in radians.
+         * @return the transformed builder.
+         */
+        public TransformBuilder rotate(double radians) {
+            double cos = Math.cos(radians);
+            double sin = Math.sin(radians);
+            double[][] operation = createIdentityMatrix().toArray();
+            operation[0][0] = cos;
+            operation[1][0] = -sin;
+            operation[0][1] = sin;
+            operation[1][1] = cos;
+            applyOperation(operation);
+            return this;
+        }
+
+        /**
+         * Shears the x-axis based on the y factor.
+         *
+         * @param y shear factor for y-axis.
+         * @return the transformed builder.
+         */
+        public TransformBuilder shearX(double y) {
+            double[][] operation = createIdentityMatrix().toArray();
+            operation[1][0] = y;
+            applyOperation(operation);
+            return this;
+        }
+
+        /**
+         * Shears the y-axis based on x factor.
+         *
+         * @param x shear factor for x-axis.
+         * @return the transformed builder.
+         */
+        public TransformBuilder shearY(double x) {
+            double[][] operation = createIdentityMatrix().toArray();
+            operation[0][1] = x;
+            applyOperation(operation);
+            return this;
+        }
+
+        /**
+         * Creates the transformation matrix.
+         *
+         * @return the transformation matrix.
+         */
+        public Mat3 build() {
+            return new Mat3(matrix);
+        }
+
+        private void applyOperation(double[][] operation) {
+            Mat3 operationMat = new Mat3(operation);
+            matrix = operationMat.multiply(matrix);
+        }
+
+    }
+}
---
+++ src/test/java/com/github/jordanpottruff/jgml/Mat3Test.java
@@ -0,0 +1,95 @@
+package com.github.jordanpottruff.jgml;
+
+import org.junit.Test;
+
+import static com.github.jordanpottruff.jgml.MatMNTest.assertMatricesEqual;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Mat3Test {
+    private static final double ERROR_MARGIN = .001;
+
+    private static final Mat3 MAT_A = new Mat3(new double[][]{{1.0, 2.0, 3.00}, {5.0, 6.0, 7.0}, {9.0, 10.0, 11.0}});
+    private static final Mat3 MAT_B = new Mat3(new double[][]{{5.0, 10.0, 15.0}, {25.0, 30.0, 35.0}, {45.0, 50.0, 55.0}});
+    private static final Mat3 MAT_C = new Mat3(new double[][]{{5.0, 1.0, 1.0}, {1.0, 5.0, 1.0}, {1.0, 1.0, 5.0}});
+
+    @Test
+    public void testInvert() {
+        Mat3 inverted = new Mat3(new double[][]{{-1.0, -2.0, -3.0}, {-5.0, -6.0, -7.0, }, {-9.0, -10.0, -11.0}});
+        assertMatricesEqual(inverted, MAT_A.invert(), ERROR_MARGIN);
+    }
+
+    @Test
+    public void testAdd() {
+        Mat3 sum = new Mat3(new double[][]{{6.0, 12.0, 18.0}, {30.0, 36.0, 42.0}, {54.0, 60.0, 66.0}});
+        assertMatricesEqual(sum, MAT_A.add(MAT_B), ERROR_MARGIN);
+    }
+
+    @Test
+    public void testSubtract() {
+        Mat3 diff = new Mat3(new double[][]{{4.0, 8.0, 12.0}, {20.0, 24.0, 28.0}, {36.0, 40.0, 44.0}});
+        assertMatricesEqual(diff, MAT_B.subtract(MAT_A), ERROR_MARGIN);
+    }
+
+    @Test
+    public void testScale() {
+        assertMatricesEqual(MAT_B, MAT_A.scale(5.0), ERROR_MARGIN);
+    }
+
+    @Test
+    public void testDeterminant() {
+        assertEquals(0.0, MAT_A.determinant(), ERROR_MARGIN);
+    }
+
+    @Test
+    public void testInverse() {
+        Mat3 inverse = new Mat3(new double[][]{{0.2143, -0.0357, -0.0357}, {-0.0357, 0.2143, -0.0357}, {-0.0357, -0.0357, 0.2143}});
+        assertMatricesEqual(inverse, MAT_C.inverse(), ERROR_MARGIN);
+    }
+
+    @Test
+    public void testBuilder_scale() {
+        Mat3 scaleMat = new Mat3.TransformBuilder().scale(5.0, 3.0).build();
+        Mat3 expected = new Mat3(new double[][]{{5.0, 0.0, 0.0}, {0.0, 3.0, 0.0}, {0.0, 0.0, 1.0}});
+        assertMatricesEqual(expected, scaleMat, ERROR_MARGIN);
+    }
+
+    @Test
+    public void testBuilder_translate() {
+        Mat3 translateMat = new Mat3.TransformBuilder().translate(10.0, -10.0).build();
+        Mat3 expected = new Mat3(new double[][]{{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {10.0, -10.0, 1.0}});
+        assertMatricesEqual(expected, translateMat, ERROR_MARGIN);
+    }
+
+    @Test
+    public void testBuilder_rotate() {
+        Mat3 rotateXMat = new Mat3.TransformBuilder().rotate(Math.PI/2).build();
+        Mat3 expected = new Mat3(new double[][]{{0.0, 1.0, 0.0}, {-1.0, 0.0, 0.0}, {0.0, 0.0, 1.0}});
+        assertMatricesEqual(expected, rotateXMat, ERROR_MARGIN);
+    }
+
+    @Test
+    public void testBuilder_shearX() {
+        Mat3 shearXMat = new Mat3.TransformBuilder().shearX(1.0).build();
+        Mat3 expected = new Mat3(new double[][]{{1.0, 0.0, 0.0}, {1.0, 1.0, 0.0}, {0.0, 0.0, 1.0}});
+        assertMatricesEqual(expected, shearXMat, ERROR_MARGIN);
+    }
+
+    @Test
+    public void testBuilder_shearY() {
+        Mat3 shearYMat = new Mat3.TransformBuilder().shearY(1.0).build();
+        Mat3 expected = new Mat3(new double[][]{{1.0, 1.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}});
+        assertMatricesEqual(expected, shearYMat, ERROR_MARGIN);
+    }
+
+    @Test
+    public void testBuilder_chainOperations() {
+        // Variety of operations
+        Mat3 transformation = new Mat3.TransformBuilder().rotate(Math.PI/4).scale(4.0, 2.0).translate(10.0, -100.0).build();
+        Mat3 expected = new Mat3(new double[][]{{2.8284, 1.4142, 0.0}, {-2.8284, 1.4142, 0.0}, {10.0, -100.0, 1.0}});
+        assertMatricesEqual(expected, transformation, ERROR_MARGIN);
+
+        // Reverse operations
+        Mat3 reverse = new Mat3.TransformBuilder().rotate(Math.PI/4).scale(5.0, 1.0).scale(1.0/5.0, 1.0).rotate(-Math.PI/4).build();
+        assertMatricesEqual(Mat3.createIdentityMatrix(), reverse, ERROR_MARGIN);
+    }
+}
